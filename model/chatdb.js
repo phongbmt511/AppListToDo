625f54278a53a78e26712eaeabbb301bc6ee2ad2
phongbmt511 <phongbmt178@gmail.com>
2023-10-19 15:23:16 +0700

    code new

--- model/chatdb.js
+++ model/chatdb.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/todolist', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-const Schema = mongoose.Schema;
-const chatSchema = new Schema({
-    username: String,
-    message: String,
-    timestamp: Date
-});
+// mongoose.connect('mongodb://127.0.0.1/todolist', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+// const Schema = mongoose.Schema;
+// const chatSchema = new Schema({
+//     username: String,
+//     message: String,
+//     timestamp: Date
+// });
 
-const Chat = mongoose.model('Chat', chatSchema);
+// const Chat = mongoose.model('Chat', chatSchema);
 
-module.exports = Chat;
+// module.exports = Chat;
