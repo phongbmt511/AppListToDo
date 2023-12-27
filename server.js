81c38e3102b8067d611cb12b03d4d1665d820764
phongbmt511 <phongbmt178@gmail.com>
2023-12-27 17:56:55 +0700

    code new

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketIO = require("socket.io");
-//const Chat = require('./model/chatdb');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-const taskRouter = require('./routers/task')
-const chatRouter = require('./routers/chat');
-app.use('/api/tasks', taskRouter)
+
 app.use('/public', express.static(path.join(__dirname, '/public')));// ep kieu du lieu
 // login
 app.get('/', (req, res) => {
@@ -23,11 +20,11 @@ app.get('/index.html', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 // chat
-app.use('/api/chat', chatRouter);
+
 app.get('/chat', (req, res) => {
     res.sendFile(__dirname + '/Chat.html')
 })
-//
+
 
 
 // su kien socket io
@@ -46,19 +43,7 @@ io.on("connection", (socket) => {
     socket.on("on-chat", (message) => {
         // xu li tin nhan va gui lai cho all user
         io.emit("user-chat", message);
-        // luu tin nhan vao mongodb
-        // const newChat = new Chat({
-        //     username: message.name,
-        //     message: message.message,
-        //     timestamp: new Date(),
-        // });
-        // newChat.save()
-        //     .then(() => {
-        //         console.log('Save mongodb success');
-        //     })
-        //     .catch(err => {
-        //         console.error('Error save mongodb', err);
-        //     });
+
     });
 });
 
